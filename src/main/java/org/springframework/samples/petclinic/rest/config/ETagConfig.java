3df8b3692dccbaeeb7417df8f09b8947c6e58c2c
K Ramesh <krameshr348@gmail.com>
2026-07-15 05:09:11 +0530

    Add ETagConfig: enable Spring's ShallowEtagHeaderFilter for automatic ETag/304 Not Modified support

---
+++ src/main/java/org/springframework/samples/petclinic/rest/config/ETagConfig.java
@@ -0,0 +1,16 @@
+package org.springframework.samples.petclinic.rest.config;
+
+import jakarta.servlet.Filter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.filter.ShallowEtagHeaderFilter;
+
+@Configuration
+public class ETagConfig {
+
+    @Bean
+    public Filter shallowEtagHeaderFilter() {
+        return new ShallowEtagHeaderFilter();
+    }
+
+}
